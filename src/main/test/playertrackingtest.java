0dc6744f118c430853c475e0d021012b29e03b31
eoin ager <G00431577@atu.ie>
2025-03-22 23:37:34 +0000

    added junit testing to test to make sure scanner is closed

--- src/main/test/playertrackingtest.java
+++ src/main/test/playertrackingtest.java
@@ -1,18 +1,27 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.Scanner;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class playertrackingtest {
 
+    Scanner scanner;
+
     @BeforeEach
     void setUp() {
-        Scanner sc=new Scanner("testing");
+        scanner =new Scanner("testing");
     }
 
     @Test
     void ScannertestIsOpen()
     {
-       assert
+        assertDoesNotThrow(scanner::nextLine, "Scanner should be open and able ot read");
+    }
+
+    @Test
+    void ScannertestIsClosed(){
+        scanner.close();
+        assertThrows(IllegalStateException.class, scanner::nextLine, "Scanner should be closed");
     }
 }
