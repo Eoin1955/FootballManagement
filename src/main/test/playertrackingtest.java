5a996fe234a94ca0542702fecf3531faa5737673
eoin ager <G00431577@atu.ie>
2025-03-22 22:45:36 +0000

    added junit testing to test to make sure scanner is opening

---
+++ src/main/test/playertrackingtest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class playertrackingtest {
+
+    @BeforeEach
+    void setUp() {
+        Scanner sc=new Scanner("testing");
+    }
+
+    @Test
+    void ScannertestIsOpen()
+    {
+       assert
+    }
+}
