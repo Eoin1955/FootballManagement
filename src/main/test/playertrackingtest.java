7e4cb1d8bb04fb3332c07eee2d9e52a5f5c7cd3c
eoin ager <G00431577@atu.ie>
2025-03-23 00:25:15 +0000

    added junit testing retrieval (done)

--- src/main/test/playertrackingtest.java
+++ src/main/test/playertrackingtest.java
@@ -3,9 +3,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ie.atu.Football_Management.DatabaseCentral;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
-import java.util.concurrent.ConcurrentLinkedQueue;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -48,7 +50,21 @@ public class playertrackingtest {
     void RetrievalTest(){
         try
         {
+            String query = "SELECT * FROM players WHERE player_name LIKE ?";
+            try(PreparedStatement preparedStatement = connection.prepareStatement(query);)
+            {
+                preparedStatement.setInt(1,1);
+                try(ResultSet resultSet = preparedStatement.executeQuery();){
 
+                        assertTrue(resultSet.next(), "should find player");
+
+                        String playerName = resultSet.getString("name");
+                        int marketValue = resultSet.getInt("market_value");
+
+                        assertNotNull(playerName, "Player name should not be null");
+                        assertTrue(marketValue > 0, "Market value should be greater than zero");
+                }
+            }
         }
         catch (Exception e)
         {
