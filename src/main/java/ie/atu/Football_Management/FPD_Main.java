e8ebd2139b61fc7a2217c4f54e42ffb6294a1e5a
HP <g00433839@atu.ie>
2025-03-25 17:58:50 +0000

    Login Feature added

--- src/main/java/ie/atu/Football_Management/FPD_Main.java
+++ src/main/java/ie/atu/Football_Management/FPD_Main.java
@@ -8,7 +8,8 @@ public class FPD_Main {
         String correctUsername = "admin";
         String correctPassword = "password123";
 
-
+        String correctTeam = "Al-Nasar";
+        String correctPlayer = "Ronaldo";
 
         Scanner scanner = new Scanner(System.in);
 
@@ -25,6 +26,43 @@ public class FPD_Main {
         } else {
             System.out.println("Invalid username or password.");
         }
+
+        System.out.println("Enter in a Team name: ");
+        String teamName = scanner.nextLine();
+
+        System.out.println("Enter in a player name:  ");
+        String playerName = scanner.nextLine();
+
+        if (teamName.equals(correctTeam) && playerName.equals(correctPlayer)) {
+            System.out.println("Team and player names match.!");
+        }else{
+            System.out.println("Invalid team or player name.");
+        }
+
+        System.out.println("Choose the following Options: ");
+        System.out.println("1. Performance");
+        System.out.println("2. Disaplinary");
+        System.out.println("3. PlayerID");
+        System.out.println("4. Player Rating");
+        int option = scanner.nextInt();
+
+        if (option == 1) {
+            System.out.println("Entering option One");
+            System.out.println("Connecting.......");
+        } else if (option == 2) {
+            System.out.println("Entering option Two");
+            System.out.println("Connecting.......");
+        } else if (option == 3) {
+            System.out.println("Entering option Three");
+            System.out.println("Connecting.......");
+        } else if (option == 4) {
+            System.out.println("Entering option Four");
+            System.out.println("Connecting.......");
+        }else{
+            System.out.println("Invalid option.");
+        }
+
+
         scanner.close();
     }
 }
